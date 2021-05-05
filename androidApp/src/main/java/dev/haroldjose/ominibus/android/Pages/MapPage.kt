7ef1ad6a878eb034f08ad4baa3a3b90b6f6286cb
Harold Jose Barros Gonçalves <haroldjose@Harolds-MacBook-Pro.local>
2021-05-04 23:11:43 -0400

    finished android pages

--- androidApp/src/main/java/dev/haroldjose/ominibus/android/Pages/MapPage.kt
+++ androidApp/src/main/java/dev/haroldjose/ominibus/android/Pages/MapPage.kt
@@ -2,42 +2,23 @@ package dev.haroldjose.ominibus.android.Pages
 
 
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.tooling.preview.Preview
-import dev.haroldjose.ominibus.android.Components.TestView
 import dev.haroldjose.ominibus.android.Pages.ui.theme.OminibusTheme
-
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.annotation.VisibleForTesting
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DirectionsBus
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -66,7 +47,7 @@ import kotlinx.coroutines.launch
 //    }
 //}
 
-private const val InitialZoom = 5f
+private const val InitialZoom = 15f
 const val MinZoom = 2f
 const val MaxZoom = 20f
 
@@ -106,7 +87,8 @@ fun MapPageContent(
             FloatingActionButton(
                 onClick = {
                     navController.navigate(MainDestinations.GET_IN_PAGE)
-                }
+                },
+                backgroundColor = MaterialTheme.colors.primary,
             ) {
                 Icon(Icons.Filled.DirectionsBus,"")
             }
@@ -140,10 +122,13 @@ private fun MapViewContainer(
     longitude: String,
     modifier: Modifier = Modifier
 ) {
+    var mapZoom by rememberSaveable(map) { mutableStateOf(InitialZoom)}
     var mapInitialized by remember(map) { mutableStateOf(false) }
     LaunchedEffect(map, mapInitialized) {
         if (!mapInitialized) {
             val googleMap = map.awaitMap()
+            googleMap.setZoom(mapZoom)
+
             val position = LatLng(latitude.toDouble(), longitude.toDouble())
             googleMap.addMarker {
                 position(position)
@@ -165,6 +150,7 @@ private fun MapViewContainer(
     AndroidView({ map }) { mapView ->
         coroutineScope.launch {
             mapView.awaitMap()
+
         }
     }
 }
